f22ad9948338bad18eaa34bda642ffa1b391110a
Dustin Podell <magicdustin14@gmail.com>
2019-09-03 02:43:16 -0700

    tested various prisma queries and mutations within node

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,10 +5,43 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466'
 })
 
-prisma.query.users(null, '{id name posts { id title }}').then((data) => {
-  console.log(JSON.stringify(data, undefined, 2))
-})
+// prisma.query.users(null, '{id name posts { id title }}').then((data) => {
+//   console.log(JSON.stringify(data, undefined, 2))
+// })
 
-prisma.query.comments(null, '{id text author {id name}}').then((data) => {
+// prisma.query.comments(null, '{id text author {id name}}').then((data) => {
+//   console.log(JSON.stringify(data, undefined, 2))
+// })
+
+// prisma.mutation.createPost({
+//   data: {
+//     title: "Testing a second post",
+//     body: "",
+//     published: false,
+//     author: {
+//       connect:{
+//         id: "ck03bw0j0006407776awncln3"
+//       }
+//     }
+//   }
+// }, '{id title body published}').then((data) => {
+//   console.log(data)
+//   return prisma.query.users(null, '{id name posts { id title published }}')
+// }).then((data) => {
+//   console.log(JSON.stringify(data, undefined, 2))
+// })
+
+prisma.mutation.updatePost({
+  where:{
+    id: "ck03n1xrf06hq07774mhkqfry"
+  },
+  data:{
+    body: "Here is the body now that the post is finished",
+    published: true
+  }
+}).then((data) => {
+  console.log(data)
+  return prisma.query.posts(null, '{id title body published author {id name}}')
+}).then((data) => {
   console.log(JSON.stringify(data, undefined, 2))
 })
