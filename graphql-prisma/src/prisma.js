7df333a0d7d725d1c306acde2f1e50e786d68650
Dustin Podell <magicdustin14@gmail.com>
2019-09-02 22:31:37 -0700

    updated babel from v6 to v7

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,6 +1,10 @@
 import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
-  typeDefs: './generated/prisma.graphql',
-  endpoint: 'localhost:4466'
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: 'http://localhost:4466'
+})
+
+prisma.query.users(null, '{id name email}').then((data) => {
+  console.log(data)
 })
