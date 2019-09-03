e266a9242c2cf6bbf6c79610e2cdac0a14d89459
Dustin Podell <magicdustin14@gmail.com>
2019-09-03 02:29:13 -0700

    Testing queries directly in node

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,6 +5,10 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466'
 })
 
-prisma.query.users(null, '{id name email}').then((data) => {
-  console.log(data)
+prisma.query.users(null, '{id name posts { id title }}').then((data) => {
+  console.log(JSON.stringify(data, undefined, 2))
+})
+
+prisma.query.comments(null, '{id text author {id name}}').then((data) => {
+  console.log(JSON.stringify(data, undefined, 2))
 })
