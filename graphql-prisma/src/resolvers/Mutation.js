b004e520fcff71be89561fd40916edebd05bf024
Dustin Podell <magicdustin14@gmail.com>
2019-10-16 04:46:22 -0700

    Added ability to change password

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,14 +1,13 @@
 import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
 import getUserId from '../utils/getUserId'
+import generateToken from '../utils/generateToken'
+import hashPassword from '../utils/hashPassword'
 
 const Mutation = {
   // USERS //////////////////////////////////////////////////////////////////////////
   async createUser(parent, args, { prisma }, info){
 
-    if(args.data.password.length < 8) throw new Error("Password must be 8 characters or longer")
-
-    const password = await bcrypt.hash(args.data.password, 10)
+    const password = await hashPassword(args.data.password)
 
     const user = await prisma.mutation.createUser({
        data: {
@@ -19,7 +18,7 @@ const Mutation = {
 
     return {
       user,
-      token: jwt.sign({userId: user.id}, 'SuperSecretTempKey')
+      token: generateToken(user.id)
     }
   },
   async login(parent, args, {prisma}, info){
@@ -36,7 +35,7 @@ const Mutation = {
 
     return {
       user,
-      token: jwt.sign({userId: user.id}, "SuperSecretTempKey")
+      token: generateToken(user.id)
     }
 
   },
@@ -48,6 +47,10 @@ const Mutation = {
   async updateUser(parent, args, { prisma, request }, info){
 
     const userId = getUserId(request)
+
+    if(typeof args.data.password === 'string'){
+      args.data.password = await hashPassword(args.data.password)
+    }
 
     return prisma.mutation.updateUser({
       data:args.data,
