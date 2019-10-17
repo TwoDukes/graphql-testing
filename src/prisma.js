4d7da177e34070fb31915b9297791c6fe446b992
Dustin Podell <magicdustin14@gmail.com>
2019-10-16 21:18:59 -0700

    Moving data to env variables

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,7 @@ import {fragmentReplacements} from './resolvers/index'
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: 'MVGqFS6fa8esqDgC',
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements
 })
 
